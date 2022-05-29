307afe27c6762dcc9907b89be344824059c7565c
kanehara <>
2022-05-29 13:30:24 +0900

    change tsx to js

--- News/screens/HomeScreen.js
+++ News/screens/HomeScreen.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native'
-import ListItem from './components/ListItem'
+import { StyleSheet, FlatList, SafeAreaView } from 'react-native'
+import ListItem from '../components/ListItem'
 import { useEffect, useState } from 'react'
 import Constants from 'expo-constants'
 import axios from 'axios'
 
 const URL = `https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${Constants.manifest.extra.newsApiKey}`
 
-export default function App() {
+export default HomeScreen = () => {
     const [articles, setArticles] = useState([])
     useEffect(() => {
         fetchArticles()
@@ -16,7 +16,6 @@ export default function App() {
     const fetchArticles = async () => {
         try {
             const response = await axios.get(URL)
-            console.log(response)
             setArticles(response.data.articles)
         } catch (error) {}
     }
