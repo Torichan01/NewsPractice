b7a92f793c090dbc4bc73a683578a5883ea7228a
kanehara <>
2022-05-30 20:51:17 +0900

    setting code

--- News/screens/ArticleScreen.js
+++ News/screens/ArticleScreen.js
@@ -1,44 +1,46 @@
-import React from 'react';
-import {StyleSheet, SafeAreaView, Text, TouchableOpacity} from 'react-native';
-import {WebView} from 'react-native-webview';
-import {connect} from 'react-redux';
-import {addClip, deleteClip} from '../store/actions/user';
+import React from 'react'
+import { StyleSheet, SafeAreaView, Text, TouchableOpacity } from 'react-native'
+import { WebView } from 'react-native-webview'
+import { connect } from 'react-redux'
+import { addClip, deleteClip } from '../store/actions/user'
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+})
 
 const ArticleScreen = (props) => {
-  const {route, addClip, deleteClip} = props;
-  const {article} = route.params;
-  return (
-    <SafeAreaView style={styles.container}>
-      <TouchableOpacity
-        onPress={() => {
-          addClip({clip: article});
-        }}>
-        <Text style={{margin: 10, fontSize: 30}}>ADD_CLIP</Text>
-      </TouchableOpacity>
+    const { route, addClip, deleteClip } = props
+    const { article } = route.params
+    return (
+        <SafeAreaView style={styles.container}>
+            <TouchableOpacity
+                onPress={() => {
+                    addClip({ clip: article })
+                }}
+            >
+                <Text style={{ margin: 10, fontSize: 30 }}>ADD_CLIP</Text>
+            </TouchableOpacity>
 
-      <TouchableOpacity
-        onPress={() => {
-          deleteClip({clip: article});
-        }}>
-        <Text style={{margin: 10, fontSize: 30}}>DELETE_CLIP</Text>
-      </TouchableOpacity>
-      <WebView source={{uri: article.url}} />
-    </SafeAreaView>
-  );
-};
+            <TouchableOpacity
+                onPress={() => {
+                    deleteClip({ clip: article })
+                }}
+            >
+                <Text style={{ margin: 10, fontSize: 30 }}>DELETE_CLIP</Text>
+            </TouchableOpacity>
+            <WebView source={{ uri: article.url }} />
+        </SafeAreaView>
+    )
+}
 
 const mapStateProps = (state) => {
-  return {
-    user: state.user,
-  };
-};
+    return {
+        user: state.user,
+    }
+}
 
-const mapDispatchToProps = {addClip, deleteClip};
-export default connect(mapStateProps, mapDispatchToProps)(ArticleScreen);
+const mapDispatchToProps = { addClip, deleteClip }
+export default connect(mapStateProps, mapDispatchToProps)(ArticleScreen)
--- News/screens/ClipScreen.js
+++ News/screens/ClipScreen.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import {StyleSheet, SafeAreaView, Text} from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
+import React from 'react'
+import { StyleSheet, SafeAreaView, Text } from 'react-native'
 
 export default ClipScreen = () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text>Clip Screen</Text>
-    </SafeAreaView>
-  );
-};
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text>Clip Screen</Text>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+})
