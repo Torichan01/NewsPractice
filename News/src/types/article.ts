7ddf3732444c4bdc41945cbf367ae36545dc844d
kanehara <>
2022-05-30 21:13:25 +0900

    News

--- News/src/types/article.ts
+++
@@ -1,6 +0,0 @@
-export type Article = {
-  title: string;
-  author: string;
-  url: string;
-  urlToImage: string;
-};
--- News/src/types/navigation.ts
+++
@@ -1,7 +0,0 @@
-import {Article} from "./article";
-
-export type RootStackParamList = {
-  Home: undefined;
-  Clip: undefined;
-  Article: {article: Article};
-};
